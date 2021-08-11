4517e915077425c47b4c441c7e661df01a1ec78f
matteo <matteo@zbmath.org>
2021-08-11 08:29:31 +0200

    issues with edit

--- src/update_zblinks_api/dlmf_scraping/historical/process_historical.py
+++ src/update_zblinks_api/dlmf_scraping/historical/process_historical.py
@@ -12,9 +12,23 @@ def csv_all_historical():
         df_new["date"] = year
         df_main = pd.concat([df_main,df_new]).drop_duplicates(keep=False)
 
-    result_csv = df_main.to_csv(f"dlmf_result.csv", index = False)
+        df_changes = pd.merge(df_main, df_edit,
+                              left_on="external_id",
+                              right_on="previous_ext_id",
+                              how="inner")
+        # df_changes = df_changes.fillna("")
+        # df_changes = df_changes.drop(columns=["external_id"])
+        # df_changes = df_changes.rename(columns={"external_id_y": "external_id"})
+        # df_changes = df_changes[["document", "external_id", "date",
+        #                          "title"]]
+        #
+        # df_main = pd.concat([df_main, df_changes]).drop_duplicates(
+        #     subset=["external_id"], keep="last")
 
-    return result_csv
+    result_csv = df_main.to_csv(f"dlmf_result.csv", index = False)
+    edit_csv = df_edit.to_csv(f"dlmf_edit.csv", index=False)
+    changes_csv = df_changes.to_csv(f"dlmf_changes.csv", index=False)
+
 
 
 if __name__ == "__main__":
